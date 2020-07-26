c71f0d2b4f1b44b9364316614454dbe52618e041
Emiliano Modula <55934049+emodula@users.noreply.github.com>
2020-07-25 21:15:42 -0300

    Add files via upload

---
+++ clases/Conexion.php
@@ -0,0 +1,23 @@
+<?php
+	
+	class conectar{
+		
+		private $servidor="localhost";
+		private $usuario="root";
+		private $password="mysql";
+		private $bd="ventas";
+		
+		public function conexion(){
+			
+			$conexion=mysqli_connect($this->servidor,
+									 $this->usuario,
+									 $this->password,
+									 $this->bd);
+			
+			return $conexion;
+		}
+	}
+
+
+
+?>
