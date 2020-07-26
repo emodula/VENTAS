bca28b967394b7af1f05dfde3108ffacb7efc584
Emiliano Modula <55934049+emodula@users.noreply.github.com>
2020-07-25 21:16:41 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<?php
+	
+	require_once "clases/Conexion.php";
+	$obj=new conectar();
+	$conexion=$obj->conexion();
+
+	$sql="SELECT * FROM usuarios WHERE email='admin'";
+	$result=mysqli_query($conexion, $sql);
+	$validar=0;
+	if (mysqli_num_rows($result)>0){
+		$validar=1;
+	}
+	
+
+
+?>
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login de usuario</title>
+<link rel="stylesheet" href="librerias/bootstrap/css/bootstrap.css">
+<script src="librerias/jquery-3.2.1.min.js"></script>
+<script src="js/funciones.js"></script>
+	
+</head>
+
+<body style="background-color: gray">
+	
+	<br><br><br>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4"></div>
+			<div class="col-sm-4">
+				<div class="panel panel-primary">
+					<div class="panel panel-heading">Sistema de ventas y gestion</div>
+					<div class="panel panel-body">
+						<form id="frmLogin">
+							<label>Usuario</label>
+							<input type="text" class="form-control input-sm" name="usuario" id="usuario">
+							<label>Password</label>
+							<input type="password" class="form-control input-sm" name="password" id="password">
+							<p></p>
+							<span class="btn btn btn-primary btn-sm" id="entrarSistema">Entrar</span>
+							<?php if(!$validar): ?>
+							<a href="registro.php" class="btn btn-danger btn-sm">Registrar</a>
+							<?php endif; ?>
+							</form>
+						</div>
+				</div>
+				</div>
+				</div>
+			</div>
+</body>
+</html>
+
+<script type="text/javascript">
+
+	$(document).ready(function(){
+		$('#entrarSistema').click(function(){
+		
+		vacios=validarFormVacio('frmLogin');
+			
+			if(vacios>0){
+				alert("Debes llenar todos los campos!");
+				return false;
+			}
+
+		datos=$('#frmLogin').serialize();
+		$.ajax({
+			type:"POST",
+			data:datos,
+			url:"procesos/regLogin/login.php",
+			success:function(r){
+				if(r==1){
+					window.location="vistas/inicio.php";
+				}else{
+					alert("No se pudo acceder");
+				}
+
+			}
+		});
+	});
+	})
+
+</script>
